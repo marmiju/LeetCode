2c856c79e82b67ac44792c0a10b9ece3e2f9e221
marmiju <mar.miju.dev@gmail.com>
2025-04-21 19:49:07 +0600

    Time: 3 ms (50.43%), Space: 19 MB (24.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}  # To store number and its index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
 
-        disc = {}
-        for i,n in enumerate(nums):
-            diff = target - n
-            if diff in disc :
-                return [disc[diff],i]
-            disc[n] = i
-        return
+                
+
+
         
