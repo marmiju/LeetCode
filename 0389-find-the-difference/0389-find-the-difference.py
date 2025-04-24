e0bd34bdbb5d6a2cbc523127740e37df7f32ea66
marmiju <mar.miju.dev@gmail.com>
2025-04-24 20:24:03 +0600

    Time: 1 ms (66.19%), Space: 18.1 MB (23.45%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -5,7 +5,7 @@ class Solution:
         count_s = Counter(s)
         count_t = Counter(t)
         
-        # The extra character is the one whose count in t exceeds that in s
+       
         for char, cnt in count_t.items():
             if cnt > count_s[char]:
                 return char
