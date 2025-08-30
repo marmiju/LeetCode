40ba88df305d50e1882889517060e299b6422f59
marmiju <mar.miju.dev@gmail.com>
2025-08-30 18:33:50 +0600

    Time: 36 ms (46.94%), Space: 34.9 MB (64.18%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,10 +5,32 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        store = []
+        slow,fast = head,head
 
-        while head != None:
-            store.append(head.val)
-            head = head.next
+        # two pointer for devide two part left and right
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
-        return store == store[::-1]
+        #now traverse list
+
+        prev = None
+        curr = slow
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        # compare 
+        left,right= head,prev
+        while right:
+            if left.val != right.val:
+                return False
+            
+            left = left.next
+            right = right.next
+        
+        return True
+
+        
