b0f174cf9e973afa1639762fdf5e69260784dc16
marmiju <mar.miju.dev@gmail.com>
2025-09-08 14:32:48 +0600

    Time: 0 ms (100%), Space: 17.7 MB (60.66%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,18 +1,26 @@
 class Solution:
-    def isSingle(self, x:int):
-        x = str(x)
-        if len(x) == 1:
-            return int(x)
-        a = 0
-        for i in x:
-            a += int(i)
-        return self.isSingle(a)
+    # def isSingle(self, x:int):
+        # x = str(x)
+        # if len(x) == 1:
+        #     return int(x)
+        # a = 0
+        # for i in x:
+        #     a += int(i)
+        # return self.isSingle(a)
         
     def addDigits(self, num: int) -> int:
         if num ==0:
-            return 0
-        # return 1 + ((num - 1) % 9)
-        return self.isSingle(num)
+             return 0
+        return 1 + ((num - 1) % 9)
+        # return self.isSingle(num)
+
+        #############################################################
+        #     here have with 2 solution one is manual with          #
+        #     recurssion and one just applying digital root         #
+        #     formula                                               #
+        #     note: commented one is manual                         #
+        #############################################################
+
             
         
         
