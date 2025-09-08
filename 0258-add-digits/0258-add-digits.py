0f27dba62ace114d1f4c740d73aec9f68062480f
marmiju <mar.miju.dev@gmail.com>
2025-09-08 14:28:06 +0600

    Time: 3 ms (16.51%), Space: 17.9 MB (17.53%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,7 +1,18 @@
 class Solution:
+    def isSingle(self, x:int):
+        x = str(x)
+        if len(x) == 1:
+            return int(x)
+        a = 0
+        for i in x:
+            a += int(i)
+        return self.isSingle(a)
+        
     def addDigits(self, num: int) -> int:
         if num ==0:
             return 0
-        return 1 + ((num - 1) % 9)
+        # return 1 + ((num - 1) % 9)
+        return self.isSingle(num)
+            
         
         
