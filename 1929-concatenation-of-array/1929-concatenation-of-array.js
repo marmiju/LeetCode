6e7ae8349f83b0ef5108f7cc08f095a2ef4c02b4
marmiju <mar.miju.dev@gmail.com>
2025-11-28 12:34:02 +0600

    Time: 0 ms (100%), Space: 58.1 MB (50.33%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,6 +3,13 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    return [...nums,...nums]
+    n = nums.length
+    ans = new Array(n*2)
+
+    for(let i=0; i<n; i++){
+        ans[i] = nums[i]
+        ans[i+n] = nums[i]
+    }
+    return ans
     
 };
