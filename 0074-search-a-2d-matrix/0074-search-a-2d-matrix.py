a6bfc808c29cb6f0cd1fa594c9fa130c166acf41
marmiju <mar.miju.dev@gmail.com>
2025-12-18 10:23:34 +0600

    Time: 0 ms (100%), Space: 17.7 MB (99.99%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,33 +1,32 @@
-from typing import List
-
 class Solution:
-    def searchValue(self, mat, tar, row) -> bool:
-        m = len(mat[0])
+    def srachValue(self, row, target):
+        n = len(row)
         st = 0
-        end = m - 1
+        end = n-1
+
         while st <= end:
-            mid = st + (end - st) // 2
-            if mat[row][mid] == tar:
-                return True
-            elif tar > mat[row][mid]:
-                st = mid + 1
+            mid = st + (end -st)//2
+            if row[mid] < target:
+                st = mid +1
+            elif row[mid] > target :
+                end = mid -1
             else:
-                end = mid - 1
+                return True
         return False
 
-    def searchMatrix(self, mat: List[List[int]], target: int) -> bool:
-        n = len(mat)      # number of rows
-        m = len(mat[0])   # number of columns
-        stRow = 0
-        endRow = n - 1
 
-        while stRow <= endRow:
-            midRow = stRow + (endRow - stRow) // 2
-            if mat[midRow][0] <= target <= mat[midRow][m - 1]:
-                return self.searchValue(mat, target, midRow)
-            elif target > mat[midRow][m - 1]:
-                stRow = midRow + 1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row = len(matrix)
+        col = len(matrix[0])
+        st_row = 0
+        end_row = row -1
+
+        while st_row <= end_row:
+            mid = st_row + (end_row-st_row)//2
+            if matrix[mid][0] <= target <= matrix[mid][col-1]:
+                return self.srachValue(matrix[mid], target)
+            elif matrix[mid][0] > target:
+                end_row = mid -1
             else:
-                endRow = midRow - 1
-
+                st_row = mid + 1
         return False
